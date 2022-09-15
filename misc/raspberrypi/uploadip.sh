61377b175bd0a841b4a83ee733462c00bbfb09ba
evan's raspi <kirkilese@gmail.com>
2022-09-15 13:07:12 -0400

    moved raspberrypi commits into own branch.

--- misc/raspberrypi/uploadip.sh
+++ misc/raspberrypi/uploadip.sh
@@ -2,7 +2,8 @@
 
 # 1. get into the git repository
 cd ~/Desktop/cs334
-git pull origin master
+git checkout raspberrypi
+git pull origin raspberrypi
 
 # 2. get the IP address into the ip.md file
 hostname -I >~/Desktop/cs334/misc/raspberrypi/ip.md
@@ -10,4 +11,4 @@ hostname -I >~/Desktop/cs334/misc/raspberrypi/ip.md
 # 2. push to the git repository
 git add -A
 git commit -m "AUTO: Raspi IP address [$(date)]"
-git push origin master
+git push origin raspberrypi 
